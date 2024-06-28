c42b62d0e0d7aadab19e11a3f1cf98ca20538b2c
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-06-28 07:49:51 +0500

    result

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char c = 'c';
-        char h = 'h';
-        char a = 'a';
-        char r = 'r';
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
         System.out.println(c);
         System.out.println(h);
         System.out.println(a);
